3f447aabaf68a3eaf6dfb1eb649e5e8856c77797
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2025-11-13 20:40:14 +0100

    class noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector("nav");
 const toggle = document.querySelector(".menu-btn")
+const body = document.querySelector("body");
 toggle.addEventListener("click", () => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
@@ -8,10 +9,13 @@ toggle.addEventListener("click", () => {
     if (isOpen) {
         nav.ariaHidden = true;
         toggle.ariaExpanded = false;
+        
     }
     else {
         nav.ariaHidden = false;
         toggle.ariaExpanded = true;
     }
+    body.classList.toggle("noscroll");
     console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    
 });
