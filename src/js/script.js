c45626cb3ef85fc550b47ae76cb19302f58da60e
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2025-11-13 20:06:10 +0100

    ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,17 @@
 const nav = document.querySelector("nav");
 const toggle = document.querySelector(".menu-btn")
 toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
     nav.ariaHidden = false;
     toggle.ariaExpanded = true;
+    if (isOpen) {
+        nav.ariaHidden = true;
+        toggle.ariaExpanded = false;
+    }
+    else {
+        nav.ariaHidden = false;
+        toggle.ariaExpanded = true;
+    }
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
 });
